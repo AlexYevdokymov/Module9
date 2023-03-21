cc9ded1425feba6d11d8e723bd899e17e8f5bb98
olye <olexiy.yevdokimov.2000@gmail.com>
2023-03-21 22:33:35 +0200

    MyStack done

--- Main.java
+++ Main.java
@@ -3,28 +3,35 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        MyStack myStack = new MyStack();
+        System.out.println(myStack.size());
+        myStack.push("Hello");
+        myStack.push("Hello");
+        myStack.push("Hello");
+        System.out.println(myStack.size());
+        System.out.println(myStack.peek());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.size());
+        System.out.println();
+
+
+
         String[] objectWords = new String[]{"Hello","world","I","am","object"};
         Integer[] objectInts = new Integer[]{3,4,26,1,6,12,0,4,2,45};
-
         MyArrayList myArrayList1 = new MyArrayList(objectWords);
         MyArrayList myArrayList2 = new MyArrayList(objectInts);
-
         System.out.println(myArrayList1.get(3));
         System.out.println(myArrayList1.get(0));
         System.out.println(myArrayList2.get(4));
         System.out.println(myArrayList2.get(2));
-
         myArrayList1.add("word");
         myArrayList1.add(432532f);
-
         System.out.println(myArrayList1.get(4));
         System.out.println(myArrayList1.get(5));
-
         System.out.println("Size of myArrayList1 = " + myArrayList1.size());
         myArrayList1.remove(3);
         System.out.println(myArrayList1.get(2) + " " + myArrayList1.get(3) + " " + myArrayList1.get(4));
         System.out.println("Size of myArrayList1 = " + myArrayList1.size());
-
         System.out.println("Size of myArrayList1 = " + myArrayList1.size());
         myArrayList1.clear();
         System.out.println("Size of myArrayList1 = " + myArrayList1.size());
--- MyStack.java
+++ MyStack.java
@@ -1,4 +1,48 @@
 package org.example;
 
 public class MyStack {
+    private Object[] objects = new Object[0];
+
+    public void push(Object value) {
+        Object[] buff = objects;
+        objects = new Object[objects.length + 1];
+        for (int i = 0; i < objects.length - 1; i++) {
+            objects[i] = buff[i];
+        }
+        objects[objects.length-1] = value;
+    }
+
+    public int size() {
+        return objects.length;
+    }
+    public void clear() {
+        objects = new Object[0];
+    }
+    public void remove(int index) {
+        Object[] buffer;
+        buffer = objects;
+        objects = new Object[buffer.length - 1];
+        for(int i = 0, j = 0; i < buffer.length; i++) {
+            if(i != index) {
+                objects[j] = buffer[i];
+                j++;
+            }
+        }
+    }
+    public Object peek() {
+        return objects[0];
+    }
+    public Object pop() {
+        Object resultPop = objects[0];
+        Object[] buffer;
+        buffer = objects;
+        objects = new Object[buffer.length - 1];
+        for(int i = 0, j = 0; i < buffer.length; i++) {
+            if(i != 0) {
+                objects[j] = buffer[i];
+                j++;
+            }
+        }
+        return resultPop;
+    }
 }
